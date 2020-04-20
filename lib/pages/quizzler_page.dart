9085c15bd938d1c1c522fb8402250f90b0516f57
Aykut Asil <aykuttasil@gmail.com>
2020-04-21 00:24:32 +0300

    refactoring

--- lib/pages/quizzler_page.dart
+++ lib/pages/quizzler_page.dart
@@ -2,7 +2,67 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class QuizzlerPage extends StatelessWidget {
+class Question {
+  String questionText;
+  bool answer;
+
+  Question(this.questionText, this.answer);
+}
+
+class QuestionPresenter {
+  var questionList = <Question>[
+    Question('Question 1', true),
+    Question('Question 2', true),
+    Question('Question 3', false),
+    Question('Question 4', true),
+    Question('Question 5', false),
+    Question('Question 6', false),
+    Question('Question 7', true),
+    Question('Question 8', true),
+    Question('Question 9', false),
+    Question('Question 10', true),
+    Question('Question 11', true),
+    Question('Question 12', false),
+    Question('Question 13', true),
+  ];
+
+  Question getQuestion(int i) {
+    return questionList[i];
+  }
+}
+
+class QuizzlerPage extends StatefulWidget {
+  @override
+  _QuizzlerPageState createState() => _QuizzlerPageState();
+}
+
+class _QuizzlerPageState extends State<QuizzlerPage> {
+  QuestionPresenter presenter = QuestionPresenter();
+  int currentQuestion = 0;
+  var scoreKeeper = <Widget>[];
+
+  nextQuestion(bool userAnswer) {
+    if (presenter.getQuestion(currentQuestion).answer == userAnswer) {
+      scoreKeeper.add(Icon(
+        Icons.check,
+        color: Colors.green,
+      ));
+    } else {
+      scoreKeeper.add(Icon(
+        Icons.remove_circle,
+        color: Colors.red,
+      ));
+    }
+
+    setState(() {
+      currentQuestion++;
+      if (currentQuestion == presenter.questionList.length) {
+        currentQuestion = 0;
+        scoreKeeper.clear();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +76,7 @@ class QuizzlerPage extends StatelessWidget {
                 child: Container(
                   alignment: Alignment.center,
                   child: Text(
-                    "Question 1",
+                    presenter.getQuestion(currentQuestion).questionText,
                     style: GoogleFonts.sourceSansPro()
                         .copyWith(color: Colors.white),
                   ),
@@ -28,7 +88,9 @@ class QuizzlerPage extends StatelessWidget {
                   height: 100,
                   child: FlatButton(
                     color: Colors.red,
-                    onPressed: () {},
+                    onPressed: () {
+                      nextQuestion(false);
+                    },
                     child: Text("FALSE"),
                   ),
                 ),
@@ -39,11 +101,16 @@ class QuizzlerPage extends StatelessWidget {
                   height: 100,
                   child: FlatButton(
                     color: Colors.green,
-                    onPressed: () {},
+                    onPressed: () {
+                      nextQuestion(true);
+                    },
                     child: Text("TRUE"),
                   ),
                 ),
               ),
+              Row(
+                children: scoreKeeper,
+              )
             ],
           ),
         ),
